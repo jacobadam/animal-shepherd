bcf3b7461b1e81ed9822102de32c6148bc90f91f
jacobadam <jacobanevitt@gmail.com>
2025-12-01 12:00:32 -0600

    create props interface and render in footer, add logo and update styling

--- src/app/[locale]/components/Footer.tsx
+++ src/app/[locale]/components/Footer.tsx
@@ -1,5 +1,41 @@
-export const Footer = () => {
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+interface FooterProps {
+  links: { label: string; href: string }[];
+}
+
+export const Footer = ({ links }: FooterProps) => {
   return (
-    <footer className="w-full bg-secondary px-6 py-4 text-center text-sm text-primary"></footer>
+    <footer className="w-full bg-secondary">
+      <div className="mx-auto max-w-7xl">
+        <div className="max-w-3xl mx-auto py-4 flex justify-center items-center">
+          <Link href="/" className="flex items-start">
+            <div className="md:px-8 py-3">
+              <div className="relative h-12 w-12">
+                <Image
+                  src="/animal-shepherd-logo.jpg"
+                  fill
+                  priority
+                  alt="Logo desktop"
+                ></Image>
+              </div>
+            </div>
+          </Link>
+        </div>
+      </div>
+
+      <nav>
+        <ul className="flex flex-col md:flex-row items-center justify-center gap-4">
+          {links.map((link) => (
+            <li key={link.label}>
+              <Link href={link.href}>{link.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </footer>
   );
 };
