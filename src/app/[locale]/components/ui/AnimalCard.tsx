a255f931f190bcb359b1f6251dc6022aa3eaa211
jacobadam <jacobanevitt@gmail.com>
2025-12-02 15:52:58 -0600

    rename AnimalCard and update prop name

--- src/app/[locale]/components/ui/AnimalCard.tsx
+++ src/app/[locale]/components/ui/AnimalCard.tsx
@@ -1,17 +1,15 @@
-"use client";
-
 import Image from "next/image";
 
 interface AnimalCardProps {
-  image: string;
+  src: string;
   alt: string;
 }
 
-export const Card = ({ image, alt }: AnimalCardProps) => {
+export const AnimalCard = ({ src, alt }: AnimalCardProps) => {
   return (
     <div className="rounded-xl bg-primary shadow-lg overflow-hidden text-center">
       <div className="relative w-full h-56">
-        <Image src={image} alt={alt} fill className="object-cover" />
+        <Image src={src} alt={alt} fill className="object-cover" />
       </div>
     </div>
   );
