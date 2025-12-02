68acee86f51b122d7e708bea96416b8b2fbcccf9
jacobadam <jacobanevitt@gmail.com>
2025-12-02 15:33:57 -0600

    add tips data file

---
+++ data/tips.ts
@@ -0,0 +1,18 @@
+type TipCardData = {
+  src: string;
+  alt: string;
+};
+
+export const tipCards: TipCardData[] = [
+  { src: "/tip1.jpg", alt: "tip 1" },
+  { src: "/tip2.jpg", alt: "tip 2" },
+  { src: "/tip3.jpg", alt: "tip 3" },
+  { src: "/tip4.jpg", alt: "tip 4" },
+  { src: "/tip5.jpg", alt: "tip 5" },
+  { src: "/tip6.jpg", alt: "tip 6" },
+  { src: "/tip7.jpg", alt: "tip 7" },
+  { src: "/tip8.jpg", alt: "tip 8" },
+  { src: "/tip9.jpg", alt: "tip 9" },
+  { src: "/tip10.jpg", alt: "tip 10" },
+  { src: "/tip11.jpg", alt: "tip 11" },
+];
