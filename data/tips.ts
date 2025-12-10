3b3e60459eb3cedeb4585607818ecad156434321
jacobadam <jacobanevitt@gmail.com>
2025-12-10 13:34:37 -0600

    add id to TipCardData

--- data/tips.ts
+++ data/tips.ts
@@ -1,18 +1,19 @@
 type TipCardData = {
+  id: number;
   src: string;
   alt: string;
 };
 
 export const tipCards: TipCardData[] = [
-  { src: "/tip1.jpg", alt: "tip 1" },
-  { src: "/tip2.jpg", alt: "tip 2" },
-  { src: "/tip3.jpg", alt: "tip 3" },
-  { src: "/tip4.jpg", alt: "tip 4" },
-  { src: "/tip5.jpg", alt: "tip 5" },
-  { src: "/tip6.jpg", alt: "tip 6" },
-  { src: "/tip7.jpg", alt: "tip 7" },
-  { src: "/tip8.jpg", alt: "tip 8" },
-  { src: "/tip9.jpg", alt: "tip 9" },
-  { src: "/tip10.jpg", alt: "tip 10" },
-  { src: "/tip11.jpg", alt: "tip 11" },
+  { id: 1, src: "/tip1.jpg", alt: "Tip #1'" },
+  { id: 2, src: "/tip2.jpg", alt: "Tip #2" },
+  { id: 3, src: "/tip3.jpg", alt: "Tip #3" },
+  { id: 4, src: "/tip4.jpg", alt: "Tip #4" },
+  { id: 5, src: "/tip5.jpg", alt: "Tip #5" },
+  { id: 6, src: "/tip6.jpg", alt: "Tip #6" },
+  { id: 7, src: "/tip7.jpg", alt: "Tip #7" },
+  { id: 8, src: "/tip8.jpg", alt: "Tip #8" },
+  { id: 9, src: "/tip9.jpg", alt: "Tip #9" },
+  { id: 10, src: "/tip11.jpg", alt: "Tip #10" },
+  { id: 11, src: "/tip11.jpg", alt: "Tip #11" },
 ];
