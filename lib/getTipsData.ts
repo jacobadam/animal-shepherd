0043c705800e27828b937db6e10ef08aa55b2a31
jacobadam <jacobanevitt@gmail.com>
2025-12-16 13:45:57 -0600

    update interface name and add id

--- lib/getTipsData.ts
+++ lib/getTipsData.ts
@@ -1,13 +1,15 @@
-interface Tips {
+interface TipsCardData {
+  id: number;
   title: string;
   heading: string;
   body: string;
 }
 
-export const getTipsData = (tips: (id: string) => string): Tips[] => {
+export const getTipsData = (tips: (id: string) => string): TipsCardData[] => {
   const tipsArray = [];
   for (let i = 1; i <= 11; i++) {
     const tipsObj = {
+      id: i,
       title: tips(`${i}.title`),
       heading: tips(`${i}.heading`),
       body: tips(`${i}.body`),
